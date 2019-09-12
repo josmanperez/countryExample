ec7e106f8f9c1013273e532c336cf14b4154de03
Josman Pérez Expósito <josman.perez@udc.es>
2019-09-12 22:15:05 +0200

    Added the initial structure of the project

---
+++ CountryExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountryExample
+//
+//  Created by Josman Pérez Expósito on 12/09/2019.
+//  Copyright © 2019 personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
