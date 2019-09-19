29d7c192297f2fbdd8b3a75c0d4bea1f83386d96
Josman Pérez Expósito <josman.perez@udc.es>
2019-09-19 19:34:14 +0200

    Added fist XCTest with success

--- CountryExampleTests/CountryExampleTests.swift
+++ CountryExampleTests/CountryExampleTests.swift
@@ -10,25 +10,122 @@ import XCTest
 @testable import CountryExample
 
 class CountryExampleTests: XCTestCase {
+    
+    let mockJson = """
+[
+  {
+    "name": "Afghanistan",
+    "topLevelDomain": [
+      ".af"
+    ],
+    "alpha2Code": "AF",
+    "alpha3Code": "AFG",
+    "callingCodes": [
+      "93"
+    ],
+    "capital": "Kabul",
+    "altSpellings": [
+      "AF",
+      "Afġānistān"
+    ],
+    "region": "Asia",
+    "subregion": "Southern Asia",
+    "population": 26023100,
+    "latlng": [
+      33.0,
+      65.0
+    ],
+    "demonym": "Afghan",
+    "area": 652230.0,
+    "gini": 27.8,
+    "timezones": [
+      "UTC+04:30"
+    ],
+    "borders": [
+      "IRN",
+      "PAK",
+      "TKM",
+      "UZB",
+      "TJK",
+      "CHN"
+    ],
+    "nativeName": "افغانستان",
+    "numericCode": "004",
+    "currencies": [
+      "AFN"
+    ],
+    "languages": [
+      "ps",
+      "uz",
+      "tk"
+    ],
+    "translations": {
+      "de": "Afghanistan",
+      "es": "Afganistán",
+      "fr": "Afghanistan",
+      "ja": "アフガニスタン",
+      "it": "Afghanistan"
+    },
+    "relevance": "0"
+  }]
+""".data(using: .utf8)!
+    
 
+    var sut: URLSession!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        sut = URLSession(configuration: .default)
     }
-
+    
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        super.tearDown()
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testCreateCountry() {
+        do {
+            let decoder = JSONDecoder()
+            let countries = try decoder.decode([Country].self, from: mockJson)
+            XCTAssertEqual(countries.count, 1)
+            XCTAssertEqual(countries.first?.name, "Afghanistan")
+            XCTAssertEqual(countries.first?.capital, "Kabul")
+            XCTAssertEqual(countries.first?.region, "Asia")
+            XCTAssertEqual(countries.first?.subregion, "Southern Asia")
+            XCTAssertEqual(countries.first?.population, 26023100)
+            
+        } catch let e {
+            XCTFail(e.localizedDescription)
         }
     }
+    
+    // Asynchronous test: success fast, failure slow
+    func testValidCallToiTunesGetsHTTPStatusCode200() {
+        // given
+        let url =
+            URL(string: "https://itunes.apple.com/search?media=music&entity=song&term=abba")
+        // 1
+        let promise = expectation(description: "Status code: 200")
+        
+        // when
+        let dataTask = sut.dataTask(with: url!) { data, response, error in
+            // then
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+                return
+            } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if statusCode == 200 {
+                    // 2
+                    promise.fulfill()
+                } else {
+                    XCTFail("Status code: \(statusCode)")
+                }
+            }
+        }
+        dataTask.resume()
+        // 3
+        wait(for: [promise], timeout: 5)
+    }
+
 
 }
